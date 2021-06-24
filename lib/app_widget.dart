50f0e8e3165fd56adbea15fe72d1290662ac41f4
nataliecosta0 <natalie.gm02@gmail.com>
2021-06-23 23:40:33 -0300

    implemented ML Kit for barcode reading

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,23 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:payflow/modules/barcode_scanner/barcode_scanner_page.dart';
 import 'package:payflow/modules/home/home_page.dart';
+import 'package:payflow/modules/insert_boleto/insert_boleto_page.dart';
 import 'package:payflow/modules/splash/splash_page.dart';
-import 'package:payflow/shared/themes/app_colors.dart';
-import 'package:payflow/modules/login/login_page.dart';
+
+import 'modules/login/login_page.dart';
+import 'shared/themes/app_colors.dart';
 
 class AppWidget extends StatelessWidget {
+  AppWidget() {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitDown,
+      DeviceOrientation.portraitUp,
+    ]);
+    SystemChrome.setSystemUIOverlayStyle(
+        SystemUiOverlayStyle(statusBarColor: AppColors.primary));
+  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Pay Flow',
       theme: ThemeData(
-        primaryColor: AppColors.primary
-      ),
+          primarySwatch: Colors.orange, primaryColor: AppColors.primary),
       initialRoute: "/splash",
       routes: {
         "/splash": (context) => SplashPage(),
         "/home": (context) => HomePage(),
-        "/login": (context) => LoginPage()
+        "/login": (context) => LoginPage(),
+        "/barcode_scanner": (context) => BarcodeScannerPage(),
+        "/insert_boleto": (context) => InsertBoletoPage()
       },
     );
-  }
+  } 
 }
---
+++ lib/shared/widgets/set_label_buttons/set_label_buttons.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:payflow/shared/themes/app_colors.dart';
+import 'package:payflow/shared/widgets/divider_vertical/divider_vertical_widget.dart';
+import 'package:payflow/shared/widgets/social_login/label_button/label_button.dart';
+
+class SetLabelButtons extends StatelessWidget {
+  final String labelPrimary;
+  final VoidCallback onTapPrimary;
+  final String labelSecondary;
+  final VoidCallback onTapSecondary;
+  final bool enablePrimaryColor;
+  const SetLabelButtons({
+    Key? key,
+    required this.labelPrimary,
+    required this.onTapPrimary,
+    required this.labelSecondary,
+    required this.onTapSecondary,
+    this.enablePrimaryColor = false,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        color: AppColors.background,
+        height: 56,
+        child: Row(
+          children: [
+            if (enablePrimaryColor) ...[
+              LabelButton.primary(
+                label: labelPrimary,
+                onPressed: onTapPrimary,
+              ),
+            ] else ...[
+              LabelButton.heading(
+                label: labelPrimary,
+                onPressed: onTapPrimary,
+              ),
+            ],
+            DividerVerticalWidget(
+              height: 56,
+            ),
+            LabelButton.heading(
+              label: labelSecondary,
+              onPressed: onTapSecondary,
+            )
+          ],
+        ));
+  }
+}
